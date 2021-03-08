5b2e88a0339b0e156ea72ea19b098b3c66403d4e
Isabel_Kasim <54857733+Isabel-Kasim@users.noreply.github.com>
2021-03-07 22:55:32 -0800

    spaceinvaders pt1

---
+++ Space Invaders/Assets/Scripts/Enemy.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Enemy : MonoBehaviour
+{
+
+    public Transform enemyTransform;
+    public float speed;
+    public TextMeshProUGUI score;
+    public TextMeshProUGUI hiscore;
+    private int scorenum = 0;
+    private int hiscorenum = 0;
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        enemyTransform = GetComponent<Transform>();
+        InvokeRepeating("Enemymovement", 0.1f, 0.3f);
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(this.gameObject);
+        scorenum++;
+        //score.text = scorenum.ToString();
+        
+    }
+
+    void Enemymovement()
+    {
+        enemyTransform.position += Vector3.right * speed;
+        foreach (Transform enemy in enemyTransform)
+        {
+            if (enemy.position.x < -10.5 || enemy.position.x > 10.5)
+            {
+                speed = -speed;
+                enemyTransform.position += Vector3.down * 0.5f;
+                return;
+            }
+            if (enemy.position.y <= -5)
+            {
+                Time.timeScale = 0;
+            }
+        }
+    }
+}
---
+++ Space Invaders/Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+  public GameObject bullet;
+    public Transform player;
+  public Transform shottingOffset;
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        player = GetComponent<Transform>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetKeyDown(KeyCode.Space))
+      {
+        GameObject shot = Instantiate(bullet, shottingOffset.position, Quaternion.identity);
+        Debug.Log("Bang!");
+
+        Destroy(shot, 3f);
+
+      }
+    }
+
+    void playermovement()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            rb.AddForce(Vector2.left);
+        }
+    }
+}
